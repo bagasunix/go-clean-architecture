6deda8dce16921034d0dadab78586fbe0e444739
Aldino Pratama <aldinopratama15@gmail.com>
2024-06-09 11:00:50 +0700

    go mod init and logger

---
+++ internal/configs/logger.go
@@ -0,0 +1,37 @@
+package configs
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func InitLogger() *zap.Logger {
+	// Konfigurasi encoder, Anda dapat menyesuaikannya sesuai kebutuhan Anda
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "ts",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+
+	// Konfigurasi core zap, ganti Level sesuai kebutuhan Anda
+	atomicLevel := zap.NewAtomicLevelAt(zap.DebugLevel)
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig),
+		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout)),
+		atomicLevel,
+	)
+
+	// Inisialisasi logger zap
+	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+	return logger
+}
