b11d8b116a0b3b8b64174fc759b993425b960741
Aldino Pratama <aldinopratama15@gmail.com>
2024-06-11 22:18:12 +0700

    query migration db

---
+++ migrations/0001_create_tables.up.sql
@@ -0,0 +1,50 @@
+CREATE TABLE "user" (
+  "id" uuid PRIMARY KEY,
+  "username" varchar,
+  "full_name" varchar,
+  "email" varchar,
+  "password" varchar,
+  "created_at" timestamp
+);
+
+CREATE TABLE "customer" (
+  "id" uuid PRIMARY KEY,
+  "full_name" varchar,
+  "username" varchar,
+  "password" varchar,
+  "hp" varchar UNIQUE,
+  "email" varchar,
+  "sex" int,
+  "created_by" uuid,
+  "created_at" timestamp
+);
+
+CREATE TABLE "order" (
+  "id" uuid PRIMARY KEY,
+  "address_id" uuid,
+  "customer_id" uuid,
+  "product_name" varchar,
+  "amount" int,
+  "status" int,
+  "created_by" uuid,
+  "created_at" timestamp
+);
+
+CREATE TABLE "address" (
+  "id" uuid PRIMARY KEY,
+  "customer_id" uuid,
+  "street_address" varchar,
+  "province" varchar,
+  "district" varchar,
+  "sub_district" varchar,
+  "zip_code" varchar,
+  "created_by" uuid,
+  "created_at" timestamp
+);
+
+ALTER TABLE "order" ADD FOREIGN KEY ("customer_id") REFERENCES "customer" ("id");
+ALTER TABLE "order" ADD FOREIGN KEY ("address_id") REFERENCES "address" ("id");
+ALTER TABLE "address" ADD FOREIGN KEY ("customer_id") REFERENCES "customer" ("id");
+ALTER TABLE "customer" ADD FOREIGN KEY ("created_by") REFERENCES "user" ("id");
+ALTER TABLE "order" ADD FOREIGN KEY ("created_by") REFERENCES "user" ("id");
+ALTER TABLE "address" ADD FOREIGN KEY ("created_by") REFERENCES "user" ("id");
